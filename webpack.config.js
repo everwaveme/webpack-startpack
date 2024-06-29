e62c4e7ddd3db6668498b54818d1ad4bd9eb8543
Farhad Sadykov <f.sadykov@yahoo.com>
2024-06-29 21:46:48 +0300

    installed copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin'); //помогает работать с html-файлами и добавляет в них ссылку на бандл
 const { CleanWebpackPlugin: CleanPlugin } = require('clean-webpack-plugin'); //очищает папку dist от старых файлов
+const CopyPlugin = require('copy-webpack-plugin'); //копирует файлы или папки в dist
 
 module.exports = {
     entry: {
@@ -30,5 +31,8 @@ module.exports = {
             template: './public/index.html'
         }),
         new CleanPlugin(),
+        new CopyPlugin({
+            patterns: [{ from: './static', to: './static' }]
+        }),
     ]
 }
