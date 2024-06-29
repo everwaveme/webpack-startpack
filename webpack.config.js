3ccd5fea402ee8811e040b253edeb96a3f12fe15
Farhad Sadykov <f.sadykov@yahoo.com>
2024-06-29 22:15:26 +0300

    installed css-loader, sass, sass-loader, style-loader and set them up

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,31 @@ module.exports = {
         }
     },
 
-    module: {},
+    module: {
+        rules: [
+            // {
+            //     test: /\.(?:js|mjs|cjs)$/,
+            //     exclude: /node_modules/,
+            //     use: {
+            //         loader: 'babel-loader',
+            //         options: {
+            //             presets: [
+            //                 ['@babel/preset-env', { targets: "defaults" }]
+            //             ]
+            //         }
+            //     }
+            // },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+        ]
+    },
+
     plugins: [
         new HtmlPlugin({
             template: './public/index.html'
